c3b04585ea25cd39361fdb263a9b38da95d0b07e
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2024-01-20 08:18:28 +0200

    Add start and dev scripts, and update dependencies in package.json

---
+++ index.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const CronJob = require("cron").CronJob;
+require("dotenv").config();
+
+const webhook = process.env.SLACK_WEBHOOK_URL;
+const bibleUrl = "https://labs.bible.org/api/?passage=random&type=json";
+
+let verse;
+
+const fetchVerse = async () => {
+  console.log("Fetching verse");
+  const { data } = await axios.get(bibleUrl);
+  return data[0];
+};
+
+const getVerse = async () => {
+  if (!verse) {
+    verse = await fetchVerse();
+  }
+  return verse;
+};
+
+const postVerse = async () => {
+  console.log("Posting verse");
+  const verse = await getVerse();
+  const res = await axios.post(webhook, {
+    text: `<!channel> *${verse.bookname} ${verse.chapter}:${verse.verse}*\n${verse.text}`,
+  });
+  return res;
+};
+
+new CronJob(
+  "15 8 * * *",
+  async function () {
+    verse = await fetchVerse();
+    // await postVerse();
+  },
+  null,
+  true,
+  "Africa/Johannesburg"
+);
